c0d877c2cff1e270f319543d59f14a7588896bba
Vinicius Santos <100625647+viniciusmtsantos@users.noreply.github.com>
2022-11-10 20:16:45 -0300

    Update requests.go

--- webapp/src/requests/requests.go
+++ webapp/src/requests/requests.go
@@ -6,12 +6,13 @@ import (
 	"webapp/src/cookies"
 )
 
-// FazerRequisicaoComAutenticacao utilizada para colocar o token na requisição
+// FazerRequisicaoComAutenticacao é utilizada para colocar o token na requisição
 func FazerRequisicaoComAutenticacao(r *http.Request, metodo, url string, dados io.Reader) (*http.Response, error) {
 	request, erro := http.NewRequest(metodo, url, dados)
 	if erro != nil {
 		return nil, erro
 	}
+
 	cookie, _ := cookies.Ler(r)
 	request.Header.Add("Authorization", "Bearer "+cookie["token"])
 
