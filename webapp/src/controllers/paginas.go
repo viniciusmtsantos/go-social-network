677784c6eb74534dbf1cef715e07e14f01f32588
Vinicius Santos <100625647+viniciusmtsantos@users.noreply.github.com>
2022-11-10 20:58:16 -0300

    Update paginas.go

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -15,17 +15,24 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// CarregarTelaDeLogin Carrega a tela de login
+// CarregarTelaDeLogin renderiza a tela de login
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := cookies.Ler(r)
+
+	if cookie["token"] != "" {
+		http.Redirect(w, r, "/home", 302)
+		return
+	}
+
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
 
-// CarregarPaginaDeCadastroDeUsuario carrega pagina do usuario
+// CarregarPaginaDeCadastroDeUsuario carrega a página de cadastro de usuário
 func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "cadastro.html", nil)
 }
 
-// CarregarPaginaPrincipal carregar pagina principal com as publicações
+// CarregarPaginaPrincipal carrega a página principal com as publicações
 func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
 	response, erro := requests.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
@@ -58,13 +65,13 @@ func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+// CarregarPaginaDeAtualizacaoDePublicacao carrega a página de edição de publicação
 func CarregarPaginaDeAtualizacaoDePublicacao(w http.ResponseWriter, r *http.Request) {
 	parametros := mux.Vars(r)
 	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
-	r.ParseForm()
-
 	if erro != nil {
 		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: erro.Error()})
+		return
 	}
 
 	url := fmt.Sprintf("%s/publicacoes/%d", config.APIURL, publicacaoID)
